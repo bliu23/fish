f4f7c891478f41b2076e7cdf9967b4fcd08035ce
Brandon Liu <bliu7654@gmail.com>
2020-08-02 21:16:15 -0700

    Create basic room and create an interactive hand

---
+++ fish-fe/src/room/Room.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hand from '../hand/Hand';
+import './Room.scss';
+
+function Room() {
+  return (
+    <div className="Room">
+      <p>Room...</p>
+      <Hand />
+    </div>
+  );
+}
+
+export default Room;
