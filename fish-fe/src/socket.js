dece2e9d3fa98c73f4a951a310ec50611731060b
Brandon Liu <bliu7654@gmail.com>
2020-08-08 12:17:59 -0700

    Add basic socket support for room

--- fish-fe/src/socket.js
+++ fish-fe/src/socket.js
@@ -1,4 +1,14 @@
-import io from "socket.io-client";
-const server = "http://localhost:3001";
+import io from 'socket.io-client';
+const server = 'http://localhost:3001';
 
 export const socket = io(server);
+
+export const subscribeToMove = (cb) => {
+  if (!socket) {
+    return true;
+  }
+
+  socket.on('move', (payload) => {
+    return cb(null, payload);
+  });
+};
