018760501c12616921e403870cc480172d21c455
Justin Liu <soccermenace@gmail.com>
2020-08-08 16:51:21 -0700

    added face card typing

---
+++ fish-be/src/main/game/face.ts
@@ -0,0 +1,23 @@
+// 4 suits
+export enum Suit {
+  Club,
+  Diamond,
+  Heart,
+  Spade,
+}
+
+// 12 face
+export enum Value {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+}
